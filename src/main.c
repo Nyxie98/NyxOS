4f39ba906bf32973c98164fe6d2aeca6545dd7a8
Nyxie98 <chebbo98@gmail.com>
2023-01-23 21:21:52 +0000

    Clear screen and gitignore

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
-void dummy_test_entrypoint()
-{}
+#define VIDEO_MEMORY    0xb8000
+#define WHITE_ON_BLACK  0x0f
+
+void clear_screen()
+{
+    int screen_size =  80*25;
+    int i;
+    char *screen = VIDEO_MEMORY;
+
+    for (i = 0; i < screen_size; i++)
+    {
+        screen[i*2] = ' ';
+        screen[i*2+1] = WHITE_ON_BLACK;
+    }
+}
 
 void main()
 {
-    char* video_memory = (char *)0xb8000;
+    char* video_memory = VIDEO_MEMORY;
+
+    clear_screen();
+    
     *video_memory = 'X';
 }

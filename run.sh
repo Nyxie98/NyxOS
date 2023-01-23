aec8ce3f99d4cf077d7461aaa852fe890c1d7277
Nyxie98 <chebbo98@gmail.com>
2023-01-23 20:48:52 +0000

    initial commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+gcc -ffreestanding -c src/main.c -o out/main.o
+
+nasm src/kernel.asm -f elf64 -o out/kernel.o
+
+ld -o out/kernel.bin -Ttext 0x1000 out/kernel.o out/main.o --oformat binary
+
+nasm src/bootsector.asm -f bin -o out/bootsector.bin
+
+cat out/bootsector.bin out/kernel.bin > out/os-image.bin
+
+qemu-system-i386 -fda out/os-image.bin
